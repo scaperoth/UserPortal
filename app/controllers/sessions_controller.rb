51959363ca057206fcb33c862586316fc35bd6ac
Matthew Scaperoth <mscapero@gwu.edu>
2015-06-09 17:58:44 -0400

    fixed error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    ?user = User.find_by(email: params[:session][:email].downcase)
-    if ?user && ?user.authenticate(params[:session][:password])
+    user = User.find_by(name: params[:session][:name].downcase)
+    if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
-      log_in ?user
-      params[:session][:remember_me] == '1' ? remember(?user) : forget(?user)
-      redirect_to ?user
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to current_user
     else
       # Log the user in and redirect to the user's show page.
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
     end
   end
