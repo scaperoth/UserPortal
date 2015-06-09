b950e61be13029415e2202d0b0a39f7b0624a4fe
scaperoth <mscapero@gwu.edu>
2015-06-09 14:31:15 -0400

    finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    if params[:id].is_number?
+      @user = User.find(params[:id])
+    else
+      @user = User.find_by_name(params[:id])
+    end
   end
 
   def new
@@ -8,9 +12,14 @@ class UsersController < ApplicationController
   end
 
   def create
+
+    username = params[:user][:email].split("@").first
+    params[:user][:name] = username
+
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
+      flash[:success] = "Welcome to the User Portal!"
       redirect_to @user
     else
       render 'new'
@@ -19,9 +28,9 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,15 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module UserPortal
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -22,5 +25,8 @@ module UserPortal
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+
+    require 'ext/string'
   end
 end
